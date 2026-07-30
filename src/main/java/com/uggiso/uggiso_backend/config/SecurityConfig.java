518a2311e718d448f91bd0a86085f0363b278615
vinithan2002 <vinithan2002@gmail.com>
2026-07-30 12:40:09 +0530

    Update SecurityConfig.java

--- src/main/java/com/uggiso/uggiso_backend/config/SecurityConfig.java
+++ src/main/java/com/uggiso/uggiso_backend/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
         http
-                .cors(cors -> {})
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
 
